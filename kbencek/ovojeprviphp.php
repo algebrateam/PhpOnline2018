3fbfdef872e910ace01d3117086e60e39270e8f2
KristijanBencek <bencek.kristijan@gmail.com>
2018-09-27 19:19:54 +0200

    vjezba Git

--- kbencek/ovojeprviphp.php
+++ kbencek/ovojeprviphp.php
@@ -1,9 +1,15 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-echo " VJEZBA GIT"
-. '';
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
