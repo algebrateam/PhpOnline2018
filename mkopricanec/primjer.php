ef6964e2b4e6ee3916a5781b4aab1bc825f58d75
Elatahin <markoantonijekoprichanec@gmail.com>
2018-09-28 20:19:49 +0200

    Primjer 28.9.

---
+++ mkopricanec/primjer.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$broj = '1';
+echo '$broj';
+$a = 3;
+$b = 5;
+$pom = $b;
+$a = $pom;
+
+$mar = 'Marko Koprichanec';
+echo '$mar';
+$marpolje=array('Marko','Koprichanec');
+echo '<br>'.$marpolje[0];
+echo '<br>'.$marpolje[0];
