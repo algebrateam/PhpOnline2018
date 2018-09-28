29ed043fc883d24e62fe48d51ea4126bc557c47f
NancyRosanda <n.rosanda7@gmail.com>
2018-09-28 22:40:52 +0200

    funkcije drugim rijecima zvane

--- nrosanda/vjezba.php
+++ nrosanda/vjezba.php
@@ -6,5 +6,39 @@
  * and open the template in the editor.
  */
 
+// primjer 0
+
+$c = 80;
+$d = 100;
+echo "$c,$d<br>";
+
+//primjer 1
+
 $naziv = "Probno";
-        echo $naziv;
+
+    echo "$naziv<br>"; 
+            
+            
+// primjer 2
+               
+$a = 3;
+$b = 5;
+$pom = $a = 5; 
+$pom2 =$b = 3;        
+echo "$pom, $pom2<br>";
+
+// primjer 3 
+
+$ime = "Nancy";
+$prezime = "Rosanda"; 
+echo ".$ime..$prezime<br>"; // ne razumijem potrebu tockica
+
+// primjer 4
+
+$z = 5;
+$z++;
+echo "$z<br>";
+$z = 5;
+$z--;
+echo "$z<br>";
+
