711eb4ef42ce28ba21769d7002f6038255412792
mrvic <mrvic.predrag@gmail.com>
2018-09-28 20:07:07 +0200

    osnovni tipovi, polja

---
+++ pmrvic/3.2.5.polja.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$polje=array(); // prazno polje
+
+$polje1=array('Tesla','Bell','Edison'); //tri elementa polja
+
+echo '<br>'.$polje1[0];  // Tesla
+echo '<br>'.$polje1[1];  // Bell
+echo '<br>'.$polje1[2];  // Edison
+echo '<br>'.$polje1[3];  // Undefined offset - greška, ne postoji element 3
+
+
+
+
+
