c563233530a37313b37e1c3b0773296fc6ac4de4
sasabacic <salezd@yahoo.com>
2018-09-30 19:27:27 +0200

    ispravljeni php

---
+++ sbacic/drugiphp.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$ime ='Sasa';
+$prezime ='Bacic';
+echo $ime ;
+echo 'prezime' ;
