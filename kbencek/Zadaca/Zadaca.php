1ab6598329383daa14a2107fcf2f02036b49a121
KristijanBencek <bencek.kristijan@gmail.com>
2018-10-01 19:43:47 +0200

    Zadaca

---
+++ kbencek/Zadaca/Zadaca.php
@@ -0,0 +1,57 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/*zadatak 2*/
+
+
+$ime = 'Kristijan';
+$prezime ='Bencek';
+
+echo 'Ime ovog polaznika je '.$ime.' '.$prezime;
+
+
+echo '<br>';
+
+#----------------------------------
+
+/*zadatak 3*/
+
+$a=5;
+
+$a++;
+
+echo $a;
+
+$a=5;
+        
+$a--;
+
+echo $a;
+
+echo '<br>';
+
+#----------------------------------
+
+/*zadatak 4*/
+
+$a = 5.0;
+var_dump($a);
+
+echo '<br>';
+
+#----------------------------------
+
+/*zadatak 5*/
+
+$a++;
+
+echo $a;
+
+
+
