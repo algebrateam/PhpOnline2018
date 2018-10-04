3a03f6889c3e5e5ff598c631edf15ecd41d047dd
Elatahin <markoantonijekoprichanec@gmail.com>
2018-10-04 18:43:25 +0200

    Samo sam sve premjestio i uredio da mi bude lakse

---
+++ mkopricanec/kontrolne_strukture_i_petlje/for.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+for ($i=1;$i<=10;$i++){
+    echo 'vrijednost i je jednaka'.$i;
+}
