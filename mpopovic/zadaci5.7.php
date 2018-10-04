7f4127fe6892dba015e3167a3e5224a7e854062d
markopopovicc <marko.popovic1@gmail.com>
2018-10-04 18:59:29 +0200

    zadatak u predavanju

---
+++ mpopovic/zadaci5.7.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * Zadatak 2.
+ */
+
+$a=2;
+$b=7;
+
+echo 'Početna vrijednost varijable $a je '.$a;
+echo '<br>Početna vrijednost varijable $b je '.$b;
+
+if ($a>$b){
+    $a++;
+}
+else {
+$b--;    
+}
+
+echo '<hr>Završna vrijednost varijable $a je '.$a;
+echo '<br>Završna vrijednost varijable $b je '.$b;
