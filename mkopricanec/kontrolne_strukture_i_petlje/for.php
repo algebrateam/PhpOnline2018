00a18acc58e3d27ca57791f046228d36a93eced6
Elatahin <markoantonijekoprichanec@gmail.com>
2018-10-04 18:52:17 +0200

    Napravio prvi zadatak

--- mkopricanec/kontrolne_strukture_i_petlje/for.php
+++ mkopricanec/kontrolne_strukture_i_petlje/for.php
@@ -7,5 +7,24 @@
  */
 
 for ($i=1;$i<=10;$i++){
-    echo 'vrijednost i je jednaka'.$i;
+    echo '<br> vrijednost i je jednaka'.$i;
+}
+
+echo '<hr>';
+#svaki drugi broj
+for ($e=0;$e<=10;$e+=2){
+    echo '<br> vrijednost e je jednaka'.$e;
+}
+
+echo '<hr>';
+#svaki drugi neparni
+for ($f=1;$f<=10;$f+=2){
+    echo '<br> vrijednost f je jednaka'.$f;
+}
+
+echo '<hr>';
+#preskoci neparne
+for ($d=1;$d<=10;$d++){
+    if($i%2!=0) continue; //ukoliko naletis na neparan broj ispali van
+    echo '<br> vrijednost d je jednaka'.$d;
 }
