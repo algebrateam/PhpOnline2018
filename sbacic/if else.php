5b5b9af462fc8e8a972690ab11aa3f7991a97de9
sasabacic <salezd@yahoo.com>
2018-10-04 20:20:06 +0200

    ifelse

---
+++ sbacic/if else.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$a = 4;
+$b = 2;
+        
+if($a < $b) {
+    echo $a.'je manji od '.$b;
+}
+
+if($a >$b) {
+    echo $a.'je vece od '.$b;
+}
