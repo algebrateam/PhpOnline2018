13823b0f407181811e5a76c980a23c246803d520
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-08 16:47:47 +0000

    Apply fixes from StyleCI

--- kbencek/4.2.1.Aritmeticki-operatori_Vjezba.php
+++ kbencek/4.2.1.Aritmeticki-operatori_Vjezba.php
@@ -1,25 +1,24 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 echo 'Vjezba aritmeticki operatori <br> <hr>';
 
-echo 5+4*5;
+echo 5 + 4 * 5;
 
 echo '<br>';
 
-echo (5+4)*5;
-
-
-echo '<br>';
-
-echo 60%2;
+echo(5 + 4) * 5;
 
 echo '<br>';
-echo 50%2;
+
+echo 60 % 2;
+
+echo '<br>';
+echo 50 % 2;
 
 echo '<br>';
 
@@ -28,16 +27,6 @@ $brojradnihmjesta = 27;
 echo 'Ako imamo'
 .$brojprijava
         .'prijava za posao <br> a trazimo '
-        .$brojradnihmjesta 
+        .$brojradnihmjesta
         .'ljudi za posao. Visak prijava nam je'
 .(int) ($brojprijava - $brojradnihmjesta);
-    
-
-
-
-
-    
-
-
-
-
--- kbencek/While_Vjezba.php
+++ kbencek/While_Vjezba.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,15 +10,10 @@ $i = 1;
 
 $konacnibroj = 100;
 
-while ($i <= $konacnibroj ) {
+while ($i <= $konacnibroj) {
     $zbroj += $i;
     echo $i.' ';
     $i++;
-    
+
     echo ' ukupna suma'.$zbroj;
 }
-
-
-
-
-
