c0b2814c1d9803a3f8b0cdb1f48b7c6e693d62f0
DubravkaOre <dubravka.oreskovic@outlook.com>
2018-10-09 20:15:24 +0200

    vjezbanje array-a i zadaca 6.7 poglavlje

--- doreskovic/5.7.2_vjezba.php
+++ doreskovic/5.7.2_vjezba.php
@@ -1,10 +1,16 @@
 <?php
+//Zadatak 5.7.2. Napisati program koji koristi dvije varijable i definirajte 
+//vrijednosti. Ako je a veća od b uvećajte a za jedan, a ako je veća b, umanjite
+//a za jedan. Na kraju ispišite obje vrijednosti
 $a=32;
-$b=30;
-if($a>$b)
-    $a++;
-else 
-    $a--;
-echo $a.'<br>'.$b;
+$b=34;
+if($a>$b){
+   $a++;  
+} 
+else{ 
+    $b--;
+}
+echo 'Varijabla a je'.' '.$a.'<br>'.'Varijabla b je'.' '.$b.'<br>';
+
 
 
---
+++ doreskovic/6_7_2_zadatak.php
@@ -0,0 +1,12 @@
+<?php
+echo'2. Zadatak u poglavlju 6.7.';
+//U polju treba promijeniti ključeve tako da umjesto
+//ključa 1 piše ime5 i td.
+echo '<br>';
+echo '<br>';
+echo 'To su sljedeća imena:<br>';
+echo '<br>';
+$imena=array('ime5'=>'Franko', 'ime4'=>'Ana', 'ime3'=>'Matija', 'ime2'=>'Danijela', 'ime1'=>'Goran');
+echo '<pre>';
+print_r($imena);
+echo'</pre>';
