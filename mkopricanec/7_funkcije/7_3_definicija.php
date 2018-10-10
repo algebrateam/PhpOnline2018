129837d6db8390f60b152392020a4dad325093dd
Elatahin <markoantonijekoprichanec@gmail.com>
2018-10-10 19:05:31 +0200

    Pukla veza

--- mkopricanec/7_funkcije/7_3_definicija.php
+++ mkopricanec/7_funkcije/7_3_definicija.php
@@ -16,3 +16,24 @@ echo '<hr>';
 
 echo 'Riječ Algebra ima '.strlen('Algebra').' znakova.';
 echo '<hr>';
+
+$polje=array('Tesla','Bell','Edison');
+
+if (is_array($polje)) {
+    foreach ($polje as $key => $value) {
+        echo '<br>Riječ '.$value.' ima '.strlen($value).' znakova';
+    }
+} else {
+    echo 'Nije array';
+}
+echo '<hr>';
+
+echo date('f');
+echo '<br>';
+echo date('d.m.y');
+echo '<br>';
+echo date('d.m.Y');
+echo '<br>';
+echo date('m.Y.d');
+echo '<br>';
+echo date('F');
