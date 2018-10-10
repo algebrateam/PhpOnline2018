d1fd074b6e4b5c26ae32e31f251808f449f1eb2d
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-10 16:11:58 +0000

    Apply fixes from StyleCI

--- doreskovic/6_7_1_zadatak.php
+++ doreskovic/6_7_1_zadatak.php
@@ -1,4 +1,5 @@
 <?php
+
 echo'1. Zadatak u poglavlju 6.7.';
 //Napisati program koji upisuje 5 imena u polje $imena
 //i ispisuje ih s pomoću petlje foreach
@@ -6,9 +7,7 @@ echo '<br>';
 echo '<br>';
 echo 'To su sljedeća imena:<br>';
 echo '<br>';
-$imena=array('Franko', 'Ana', 'Matija', 'Danijela', 'Goran');
-foreach($imena as $key)
-{   
- echo $key.'<br>'; 
+$imena = ['Franko', 'Ana', 'Matija', 'Danijela', 'Goran'];
+foreach ($imena as $key) {
+    echo $key.'<br>';
 }
-
--- doreskovic/vjezba_array.php
+++ doreskovic/vjezba_array.php
@@ -1,6 +1,7 @@
 <?php
+
 echo'Prvi primjer print_r';
-$polje=array('Tesla', 'Edison', 'Bell');   
+$polje = ['Tesla', 'Edison', 'Bell'];
 echo '<pre>';
 print_r($polje);
 echo'</pre>';
@@ -8,41 +9,38 @@ echo '<hr>';
 echo'<br>';
 echo'Drugi primjer for: <br>';
 echo'<br>';
-for($i=0;$i<=2;$i++)
-{
+for ($i = 0; $i <= 2; $i++) {
     echo $polje[$i].'<br>';
 }
 echo '<hr>';
 echo'<br>';
 echo'Treći primjer for_count: <br>';
 echo'<br>';
-for($i=0;$i<count($polje);$i++)
-{
+for ($i = 0; $i < count($polje); $i++) {
     echo $polje[$i].'<br>';
 }
 echo '<hr>';
 echo'<br>';
 echo'Treći primjer foreach: <br>';
 echo'<br>';
-foreach($polje as $ime)
-{
+foreach ($polje as $ime) {
     echo $ime.'<br>';
 }
 echo '<hr>';
 echo'<br>';
 echo'Sortiranje vrijednosti polja sort: <br>';
 echo'<br>';
-$fruits=array(0=>'limun','a'=>'naranca',1=>'banana','b'=>'jabuka');
+$fruits = [0=>'limun', 'a'=>'naranca', 1=>'banana', 'b'=>'jabuka'];
 sort($fruits);
 echo '<pre>';
 print_r($fruits);
 echo'</pre>';
-    echo "<br>";
+    echo '<br>';
 echo '<hr>';
 echo'<br>';
 echo'Sortiranje vrijednosti polja asort: <br>';
 echo'<br>';
-$fruits=array(0=>'limun','a'=>'naranca',1=>'banana','b'=>'jabuka');
+$fruits = [0=>'limun', 'a'=>'naranca', 1=>'banana', 'b'=>'jabuka'];
 asort($fruits);
 echo '<pre>';
 print_r($fruits);
@@ -51,7 +49,7 @@ echo '<hr>';
 echo'<br>';
 echo'Sortiranje vrijednosti polja rsort: <br>';
 echo'<br>';
-$fruits=array(0=>'limun','a'=>'naranca',1=>'banana','b'=>'jabuka');
+$fruits = [0=>'limun', 'a'=>'naranca', 1=>'banana', 'b'=>'jabuka'];
 rsort($fruits);
 echo '<pre>';
 print_r($fruits);
@@ -60,7 +58,7 @@ echo '<hr>';
 echo'<br>';
 echo'Sortiranje vrijednosti polja rsort: <br>';
 echo'<br>';
-$fruits=array(0=>'limun','a'=>'naranca',1=>'banana','b'=>'jabuka');
+$fruits = [0=>'limun', 'a'=>'naranca', 1=>'banana', 'b'=>'jabuka'];
 ksort($fruits);
 echo '<pre>';
 print_r($fruits);
@@ -69,9 +67,8 @@ echo '<hr>';
 echo'<br>';
 echo'Prebrojavanje elemenata polja: <br>';
 echo'<br>';
-$br_elem=0;
-foreach($fruits as $key)
-{
+$br_elem = 0;
+foreach ($fruits as $key) {
     $br_elem++;
 }
 echo 'Polje ima:'.' '.$br_elem.' elementa';
@@ -79,12 +76,10 @@ echo '<hr>';
 echo'<br>';
 echo'Pretraživanje polja: <br>';
 echo'<br>';
-$br_elem=0;
-foreach($fruits as $key => $value)
-{
-    if($value=='banana')
-    {
-    break ;   
+$br_elem = 0;
+foreach ($fruits as $key => $value) {
+    if ($value == 'banana') {
+        break;
     }
 }
 echo 'U nizu je'.' '.$value.' tražena vrijednost';
