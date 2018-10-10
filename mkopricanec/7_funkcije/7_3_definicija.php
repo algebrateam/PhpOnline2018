6758a739668d6e34212e11b335459841f084981b
Elatahin <markoantonijekoprichanec@gmail.com>
2018-10-10 18:51:34 +0200

    Pratim nastavu

---
+++ mkopricanec/7_funkcije/7_3_definicija.php
@@ -0,0 +1,18 @@
+<?php
+
+  /* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo 'primjer funkcije <hr>';
+
+echo ('primjer funkcije <hr>');
+
+$timestamp=time();
+echo $timestamp;
+echo '<hr>';
+
+echo 'Riječ Algebra ima '.strlen('Algebra').' znakova.';
+echo '<hr>';
