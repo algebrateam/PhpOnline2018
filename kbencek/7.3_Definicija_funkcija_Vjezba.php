bd778f686090e65cf75b2d3577b911290c9ef311
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-10 19:45:49 +0000

    Apply fixes from StyleCI

--- kbencek/7.3_Definicija_funkcija_Vjezba.php
+++ kbencek/7.3_Definicija_funkcija_Vjezba.php
@@ -1,42 +1,41 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-echo 'Primjer funkcija<br>'; 
+echo 'Primjer funkcija<br>';
 
-echo ('Primjer funkcija<br>'); 
+echo 'Primjer funkcija<br>';
 
-$timestamp= time();
-  echo $timestamp ; 
+$timestamp = time();
+  echo $timestamp;
 
   echo '<br>';
-  
-echo 'Ime Kristijan ima' .strlen ('Kristijan'). 'slova';
+
+echo 'Ime Kristijan ima'.strlen('Kristijan').'slova';
 
 echo '<br>';
 
-$polje=array('Audi' , 'Bmw', 'Fiat');
+$polje = ['Audi', 'Bmw', 'Fiat'];
 
-    if (is_array ($polje)) {
-
-        
-foreach ($polje as $key => $value) {
-    echo '<br>Riječ' .$value. 'ima' . strlen($value). 'znakova';
-}
+    if (is_array($polje)) {
+        foreach ($polje as $key => $value) {
+            echo '<br>Riječ'.$value.'ima'.strlen($value).'znakova';
+        }
     } else {
-        echo 'Nije array';   
-}
+        echo 'Nije array';
+    }
 
-echo '<br>?' .date('F');
-echo '<br>?' .date('d.m.Y');
-echo '<br>?' .date('d.m.Y');
-echo '<br>?' .date('m.Y.d');
+echo '<br>?'.date('F');
+echo '<br>?'.date('d.m.Y');
+echo '<br>?'.date('d.m.Y');
+echo '<br>?'.date('m.Y.d');
 
-function hr_dan(){
+function hr_dan()
+{
     switch (date('W')) {
         case 0: return 'Nedjelja';
             break;
@@ -50,35 +49,29 @@ function hr_dan(){
             break;
             case 5: return 'Pet';
             echo '<br>danas je'.strtolower(hr_dan());
-                
+
 }
 }
 
 echo '<hr>Ispis HTML tablice pomocu funkcije';
 
-$boje = [['#21302' , 'bijela' , '55%'],
-        ['#212202' , 'Crvena' , '44%'],
-        ['#212202' , 'zuta' , '44%'],
-        ['#212202' , 'Plava' , '44%'],
-        ['#212202' , 'Crvena' , '44%']];
+$boje = [['#21302', 'bijela', '55%'],
+        ['#212202', 'Crvena', '44%'],
+        ['#212202', 'zuta', '44%'],
+        ['#212202', 'Plava', '44%'],
+        ['#212202', 'Crvena', '44%'], ];
 
-    function ispistablice($b){
+    function ispistablice($b)
+    {
         echo'<table borfer=1>';
-        foreach ($b as $boja){
-        echo '<tr>';
-        foreach ($boja as $v){
-            echo '<td>' .$v. '</td>';
-        }
-        echo '</tr>';
-        
+        foreach ($b as $boja) {
+            echo '<tr>';
+            foreach ($boja as $v) {
+                echo '<td>'.$v.'</td>';
+            }
+            echo '</tr>';
         }
         echo '</table>';
     }
 
     ispistablice($boje);
-
-    
-        
-    
-   
- 
--- kbencek/7.4_Globalne_funkcije_Vjezba.php
+++ kbencek/7.4_Globalne_funkcije_Vjezba.php
@@ -1,23 +1,19 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-$a=3; //globalna varijabla
+$a = 3; //globalna varijabla
 
-function inc_a(){
-   global $a; //lokalna varijabla
-   
+function inc_a()
+{
+    global $a; //lokalna varijabla
+
     echo $a;
 }
 
-
-
 echo $a;
 inc_a();
-
-
-
