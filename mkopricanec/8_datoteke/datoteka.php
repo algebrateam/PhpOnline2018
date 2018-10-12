e8e16c575937b54b3d5848c862dc616e3a387e67
Elatahin <markoantonijekoprichanec@gmail.com>
2018-10-12 20:58:59 +0200

    Pratim nastavu

---
+++ mkopricanec/8_datoteke/datoteka.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  Ime:<br>
+  <input type="text" name="ime">
+  <br>
+  Prezime:<br>
+  <input type="text" name="prezime">
+  <br>
+  Starost:<br>
+  <input type="number" name="godine"
+  <br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
