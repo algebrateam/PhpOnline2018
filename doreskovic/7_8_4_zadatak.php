d74afef0f486d5f735a6b913d10c2b4bbfb2cdde
DubravkaOre <dubravka.oreskovic@outlook.com>
2018-10-12 17:44:48 +0200

    7.8. Zadaca

---
+++ doreskovic/7_8_4_zadatak.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * Napisati funkciju koja prima varijabilni broj parametara koji su 
+ * kombinirani brojevi i stringovi. Za svaki parametar ispiši kojeg je tipa.
+ */
+function param()
+{
+    $parametars = func_get_args();
+    foreach($parametars as $param)
+    {
+        if(is_int($param)) {
+            echo $param.' '.'je broj.</br>';
+        }
+        else {
+            echo $param.' '.'je string.</br>';
+        }
+    }
+}
+echo param("Ana", "Vesna", 34, 35);
