6293197c723dec5fafb94f25215da5105dd0819c
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-12 09:46:57 +0000

    Apply fixes from StyleCI

--- kbencek/Rijesenje.7.7.php
+++ kbencek/Rijesenje.7.7.php
@@ -5,11 +5,10 @@ function mini($a = 5, $b = 7)
     echo '<hr>'.min(func_get_args());
 }
 $ime = 'mini';
-switch ($ime){
+switch ($ime) {
     case 'mini':
         mini(5, 7);
         break;
 }
 
 // Koristili smo funkciju func_get_args a nismo nigdje definirali mini
-
