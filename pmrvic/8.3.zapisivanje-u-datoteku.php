5cd8e2ee72649a01a389c001f115cc96a807d5a3
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-12 18:59:00 +0000

    Apply fixes from StyleCI

--- pmrvic/8.3.zapisivanje-u-datoteku.php
+++ pmrvic/8.3.zapisivanje-u-datoteku.php
@@ -1,12 +1,12 @@
 <?php
 
-/* 
+/*
  * @author PMrvic
  * @example
  * $myfile = fopen("webdictionary.txt", "w") or die("Unable to open file!");
  * echo fwrite($myfile,filesize("webdictionary.txt"));
  * fclose($myfile);
- * 
+ *
  * Modes	Description
  * r	Open a file for read only. File pointer starts at the beginning of the file
  * w	Open a file for write only. Erases the contents of the file or creates a new file if it doesn't exist. File pointer starts at the beginning of the file
@@ -28,19 +28,17 @@
 // '/msvraka/polaznici.txt';  --> apsolutna putanja
 
 // $filename='./polaznici.txt';  // najispravnije
-$filename='./test.txt';  // najispravnije
-$mode='a+';
+$filename = './test.txt';  // najispravnije
+$mode = 'a+';
 
-$handle= fopen($filename, $mode);  //$handle je tipa 'Resource'
+$handle = fopen($filename, $mode);  //$handle je tipa 'Resource'
 
 //echo $handle;  // ispisuje 'Resource id #3' -> beskorisno
 
-
 // fread(<Resource>,<int>);  // int duljina u bajtovima
 //$contents=fread($handle, filesize($filename));
-for($i=0;$i<=200;$i++){
-   fwrite($handle, $i.' Ovo smo upisali u datoteku test.txt'."\n"); 
+for ($i = 0; $i <= 200; $i++) {
+    fwrite($handle, $i.' Ovo smo upisali u datoteku test.txt'."\n");
 }
 
-fclose($handle);  // obavezno zatvori stream 
-
+fclose($handle);  // obavezno zatvori stream
