e394e12946be72f9846c3065705f1063bc6ef03b
NancyRosanda <n.rosanda7@gmail.com>
2018-10-14 16:55:17 +0200

    zadaca

--- nrosanda/zadaca.php
+++ nrosanda/zadaca.php
@@ -3,10 +3,10 @@ if (isset($_POST['potvrdi'])) {
     $filename = './data.txt';
     $mode = 'a+';
     $handle = fopen($filename, $mode);
-    fwrite($handle, $_POST['broj']);
-    fwrite($handle, $_POST['ime']);
-    fwrite($handle, $_POST['datum']);
-    fwrite($handle, $_POST['odluka']);
+    fwrite($handle, $_POST['broj'] . "\n");
+    fwrite($handle, $_POST['ime'] . "\n");
+    fwrite($handle, $_POST['datum'] . "\n");
+    fwrite($handle, $_POST['odluka'] . "\n");
     fclose($handle);
 }
 $dat = file('./data.txt');
@@ -29,7 +29,7 @@ and open the template in the editor.
         <form method="post">
             Broj: <input type="number" name="broj" title="Unesi broj">
             Ime: <input type="text" name="ime" title="Unesi ime">
-            Datum: <input value="16.Ožujak.2018.">
+            Datum: <input type="date" name="datum" value="16.Ožujak.2018.">
             Odluka: <select name="odluka"> 
                 <option>Pad</option>
                 <option>Prolaz</option>
@@ -38,12 +38,37 @@ and open the template in the editor.
 
             <?php
             if (file_exists($filename)) {
-                echo '<table border=1>';
-                foreach ($dat as $red) {                   
-                    echo '<tr><td>'.$red.'</td></td>';
-                    }
+                echo '<table border="1" width="300">';
+                echo '<thead>';
+                echo '<tr>';
+                echo '<th>';
+                echo 'Broj';
+                echo '</th>';
+                echo ' <th>Ime</th>';
+                echo '<th>Datum</th>';
+                echo '<th>';
+                echo 'Odluka';
+                echo '</th>';
+                echo '</tr>';
+                echo '</thead>';
+                echo '<tbody>';
+                echo ' <tr>';
+                echo '<td>';
+                echo $_POST['broj'];
+                echo '</td>';
+                echo '<td>';
+                echo $_POST['ime'];
+                echo '</td>';
+                echo '<td>';
+                echo '16.Ožujak.2018.';
+                echo '</td>';
+                echo '<td>';
+                echo $_POST['odluka'];
+                echo '</td>';
+                echo '</tr>';
+                echo '</tbody>';
+                echo '</table>';
             }
-            echo '</table>';
             ?>
         </form> 
 
