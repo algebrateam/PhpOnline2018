c5d2565391b5dfe9982ff8fa049ff5f8b8e1bc32
mrvic <mrvic.predrag@gmail.com>
2018-10-16 21:11:56 +0200

    Nagradni Z astra-zafira dopuna

--- pmrvic/class/primjer-astra-zafira.php
+++ pmrvic/class/primjer-astra-zafira.php
@@ -3,14 +3,11 @@
 include './Astra.php';
 include './Zafira.php';
 
-<<<<<<< HEAD
+
 $A1=new Astra();
 $A1->boja='Plava';
 $Z1= new Zafira();
-=======
-$A1 = new Astra();
-$Z1 = new Zafira();
->>>>>>> 0af81ea12d48467a1829bf8e438a737ae4048a72
+
 
 echo '<h1>Usporedba vozila</h1>';
 
