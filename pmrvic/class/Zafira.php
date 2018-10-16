4a98788914a5cdeacf1bd4f39e04e3cfc739bd07
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-16 19:09:50 +0000

    Apply fixes from StyleCI

--- pmrvic/class/Zafira.php
+++ pmrvic/class/Zafira.php
@@ -7,6 +7,6 @@ class Zafira extends Opel implements IZafira
     public function opcija7()
     {
         // popstavi da vouzilo ima 7 sjedala !
-        $this->brojsjedala=7;
+        $this->brojsjedala = 7;
     }
 }
