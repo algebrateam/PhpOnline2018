08511c5c99b7765dc763696a67aa889ed60c1da7
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-18 19:04:47 +0000

    Apply fixes from StyleCI

--- pmrvic/class/grafika_objektima/Draw.php
+++ pmrvic/class/grafika_objektima/Draw.php
@@ -7,14 +7,12 @@ require_once './class/Linija.php';
 
 use primar\Tocka;
 
-$T1= new Tocka(0,0);
-$T2= new Tocka(3,4);
+$T1 = new Tocka(0, 0);
+$T2 = new Tocka(3, 4);
 
-echo "<br>T1:".$T1->tostring();
-echo "<br>T2:".$T2->tostring();
+echo '<br>T1:'.$T1->tostring();
+echo '<br>T2:'.$T2->tostring();
 
-echo "<hr>";
-$L1= new Linija($T1,$T2);
+echo '<hr>';
+$L1 = new Linija($T1, $T2);
 echo $L1->tostring();
-
-
--- pmrvic/class/grafika_objektima/autoload.php
+++ pmrvic/class/grafika_objektima/autoload.php
@@ -1,9 +1,8 @@
 <?php
 
-/* 
+/*
  * http://php.net/manual/en/function.spl-autoload.php
  */
-
 
     // Your custom class dir
     define('CLASS_DIR', 'class/');
@@ -11,11 +10,9 @@
     // Add your class dir to include path
     //set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
     set_include_path($_SERVER['DOCUMENT_ROOT'].PATH_SEPARATOR.CLASS_DIR);
-    
+
     // You can use this trick to make autoloader look for commonly used "My.class.php" type filenames
     spl_autoload_extensions('.php');
 
     // Use default autoload implementation
     spl_autoload_register();
-
-
