03ca7efcec95e84c8e4c1768ed1baed2f6f69670
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-18 17:29:08 +0000

    Apply fixes from StyleCI

--- pmrvic/class/primjer_autoload/class/Macke/Cat.class.php
+++ pmrvic/class/primjer_autoload/class/Macke/Cat.class.php
@@ -2,14 +2,18 @@
 
 namespace Macke{
 
-class Cat {
-private $name;
+class Cat
+{
+    private $name;
 
-  static function says() {
-    return 'meoow';
-  }
-  public function prede(){
-    return "Prrrrrr";
-  }
+    public static function says()
+    {
+        return 'meoow';
+    }
+
+    public function prede()
+    {
+        return 'Prrrrrr';
+    }
 }
 }
