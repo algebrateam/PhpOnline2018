a1fe184c1681a10094e1085b2fe35527aa9ef7aa
mrvic <mrvic.predrag@gmail.com>
2018-10-18 21:39:15 +0200

    ispravljen autoloading

--- pmrvic/class/grafika_objektima/autoload.php
+++ pmrvic/class/grafika_objektima/autoload.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /* 
@@ -21,23 +20,3 @@
     spl_autoload_register();
 
 
-=======
-<?php
-
-/*
- * http://php.net/manual/en/function.spl-autoload.php
- */
-
-    // Your custom class dir
-    define('CLASS_DIR', 'class/');
-
-    // Add your class dir to include path
-    //set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
-    set_include_path($_SERVER['DOCUMENT_ROOT'].PATH_SEPARATOR.CLASS_DIR);
-
-    // You can use this trick to make autoloader look for commonly used "My.class.php" type filenames
-    spl_autoload_extensions('.php');
-
-    // Use default autoload implementation
-    spl_autoload_register();
->>>>>>> e743adf92d1bfc67dab11dac62d9186c53d15cb5
--- pmrvic/class/grafika_objektima/primar/Tocka.php
+++ pmrvic/class/grafika_objektima/primar/Tocka.php
@@ -1,15 +1,5 @@
 <?php
-<<<<<<< HEAD:pmrvic/class/grafika_objektima/primar/Tocka.php
 namespace primar;
-class Tocka implements ITocka{
-    private $x=0;  // int
-    private $y=0;  // int
-    
-    function __construct($x,$y) {
-        $this->x=$x;
-        $this->y=$y;
-=======
-
 class Tocka implements ITocka
 {
     private $x = 0;  // int
@@ -19,7 +9,6 @@ class Tocka implements ITocka
     {
         $this->x = $x;
         $this->y = $y;
->>>>>>> e743adf92d1bfc67dab11dac62d9186c53d15cb5:pmrvic/class/grafika_objektima/class/Tocka.php
     }
 
     public function setx($x)
