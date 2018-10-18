34731bc7c6e2f9a400ad80ace81c7bf58d56a124
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-10-18 16:59:33 +0000

    Apply fixes from StyleCI

--- pmrvic/class/auti/Astra.php
+++ pmrvic/class/auti/Astra.php
@@ -4,16 +4,19 @@ require_once './Opel.php';
 require_once './IAstra.php';
 class Astra extends Opel implements IAstra
 {
-    public $opis='The Opel Astra (Lat: Stars) is a '
-        . 'compact car/small family car (C-segment in Europe) '
-        . 'engineered and manufactured by the German automaker Opel since 1991.';
-    private $slika='astra.jpg';
-    public function ispisslike(){
-        if (isset($this->slika)){
+    public $opis = 'The Opel Astra (Lat: Stars) is a '
+        .'compact car/small family car (C-segment in Europe) '
+        .'engineered and manufactured by the German automaker Opel since 1991.';
+    private $slika = 'astra.jpg';
+
+    public function ispisslike()
+    {
+        if (isset($this->slika)) {
             return '<img src="'.$this->slika.'" >';
         }
     }
-     public function ispis()
+
+    public function ispis()
     {
         return ' '
         .$this->brojsjedala
@@ -23,8 +26,8 @@ class Astra extends Opel implements IAstra
             .$this->opis
             .' '
             .$this->ispisslike();
-        
     }
+
     public function stedljiva3()
     {
         $this->stedljiva = 3;
--- pmrvic/class/auti/Zafira.php
+++ pmrvic/class/auti/Zafira.php
@@ -4,19 +4,22 @@ require_once './Opel.php';
 require_once './IZafira.php';
 class Zafira extends Opel implements IZafira
 {
-        public $opis='The Opel Zafira, also known as the Zafira Tourer '
-            . 'since 2011,[1] is a multi-purpose vehicle (MPV) '
-            . 'produced by the German manufacturer Opel since the beginning of 1999.';
-    private $slika='zafira.jpg';
-    public function ispisslike(){
-        if (isset($this->slika)){
+    public $opis = 'The Opel Zafira, also known as the Zafira Tourer '
+            .'since 2011,[1] is a multi-purpose vehicle (MPV) '
+            .'produced by the German manufacturer Opel since the beginning of 1999.';
+    private $slika = 'zafira.jpg';
+
+    public function ispisslike()
+    {
+        if (isset($this->slika)) {
             return '<img src="'.$this->slika.'" >';
         }
     }
-         public function ispis()
+
+    public function ispis()
     {
         return ' '
-        
+
             .' <br>boja: '
             .$this->boja
             .' <strong>'
@@ -25,8 +28,8 @@ class Zafira extends Opel implements IZafira
             .$this->ispisslike()
             .'<br>broj sjedala:  '
             .$this->brojsjedala;
-        
     }
+
     public function opcija7()
     {
         // popstavi da vouzilo ima 7 sjedala !
