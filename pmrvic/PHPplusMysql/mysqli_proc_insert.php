77c8a248b83b3e4377dba903fa0410d489b58ba0
mrvic <mrvic.predrag@gmail.com>
2018-11-21 19:22:29 +0100

    mysqli insert i error handling

---
+++ pmrvic/PHPplusMysql/mysqli_proc_insert.php
@@ -0,0 +1,41 @@
+<?php
+include_once './dbconn_proc.php';
+?>
+<!DOCTYPE html>
+<html>
+<body>
+ 
+    
+    <?php
+       /*
+    [mbrStud] => 1120
+    [imeStud] => Zdenko
+    [prezStud] => Kolac
+    [pbrRod] => 31000
+    [pbrStan] => 40000
+    [datRodStud] => 1985-06-06 00:00:00
+    [jmbgStud] => 0606985330186
+    */
+    
+      $query ="INSERT INTO stud(mbrStud,imeStud,prezStud,pbrRod,pbrStan,datRodStud,jmbgStud)";
+      $query .="VALUES(1521,'George','Washington',10000,10000,'1985-06-06 00:00:00','0606985330177')";
+      
+      $result= mysqli_query($link, $query);
+      
+      echo "broj insertanih / update / ili delete redova:". mysqli_affected_rows($link)."<br>";
+      
+      echo mysqli_errno($link); //	Returns the last error code for the most recent function call
+      echo "<pre>";
+      print_r( mysqli_error_list($link));//	Returns a list of errors for the most recent function call
+       echo "</pre>";
+      echo mysqli_error($link); //Returns the last error description for the most recent function call
+      
+      
+      
+    ?>
+      
+</body>
+
+</html>
+
+
--- pmrvic/PHPplusMysql/mysqli_proceduralni.php
+++ pmrvic/PHPplusMysql/mysqli_proceduralni.php
@@ -5,7 +5,7 @@ include_once './dbconn_proc.php';
 <!DOCTYPE html>
 <html>
   <head>
-    <title>TODO supply a title</title>
+    <title>Popis studenata mysqli proceduralnim pristupom</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
@@ -13,9 +13,27 @@ include_once './dbconn_proc.php';
     <div>
       <?php
       $query ="SELECT * FROM stud LIMIT 50";
+      
       $result= mysqli_query($link, $query);
+      echo "<pre>";
+      var_dump($result);
+      
+      print_r($result);
+      
+      $row = mysqli_fetch_assoc($result);
+       print_r($row);
+      echo "</pre><hr>";
+       echo "<br>Broj redova vraćenih querijem:".$result->num_rows;
+       echo "<br>Broj redova vraćenih querijem:".mysqli_num_rows($result);
+       
+       function vrati_broj_redova($r){
+           return $r->num_rows;
+       }
+       echo "<br>Broj redova vraćenih querijem:".vrati_broj_redova($result);
+       echo "<hr>";
+      
       while ($row = mysqli_fetch_assoc($result)) {
-          echo $row['imeStud']." ".$row['prezStud'];
+          echo $row['imeStud']." ".$row['prezStud']. " ".$row['mbrStud']."<br>";
       }
     ?>
     
